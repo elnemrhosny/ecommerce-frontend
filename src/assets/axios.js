4e81e0ca3cf4a2dc64d55491b1e5e12bd6bacafa
elnemrhosny <elnemrhosny@gmail.com>
2026-08-17 02:38:35 +0300

    Initial commit

--- src/assets/axios.js
+++ src/assets/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "http://localhost:8000" , 
+    baseURL : "ecommerce-backend-seven-wine.vercel.app" , 
     timeout : 10000 , 
     headers : {
         'Content-Type' : 'application/json' ,
